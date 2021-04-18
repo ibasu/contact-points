beeba5856ee03f90ac0ae002957166abd905c1ea
idbasu <indranil_db@yahoo.com>
2021-04-18 22:01:15 +1000

    Updated code

--- src/main/java/au/com/belong/customermaster/contactpoints/exception/ResourceNotFoundException.java
+++ src/main/java/au/com/belong/customermaster/contactpoints/exception/ResourceNotFoundException.java
@@ -5,8 +5,4 @@ public class ResourceNotFoundException extends BaseException {
     public ResourceNotFoundException(String message){
         super(message);
     }
-
-    public ResourceNotFoundException(String message, Throwable t){
-        super(message, t);
-    }
 }
